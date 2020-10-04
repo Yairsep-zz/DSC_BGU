d1ea86f019f3ed186be2a13d40ce67d18b7d1e0d
ESTIBA <estibaraness696@gmail.com>
2020-10-04 15:34:30 +0300

    PartnerCard - design

--- client/src/pages/Partners/PartnersCard/PartnerCard.jsx
+++ client/src/pages/Partners/PartnersCard/PartnerCard.jsx
@@ -4,10 +4,8 @@ import {Avatar, Card, CardContent, CardMedia} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 
 const PartnerCard = (props)=>{
-
     const {partner} = props;
     const {name, logo , url} = partner;
-
     return(
         <div className={'PartnerCard'} >
             <Card className={'card'} elevation={5}>
@@ -15,8 +13,8 @@ const PartnerCard = (props)=>{
                     <Avatar alt={name} src={logo} className={'avatar'}/>
                 </CardMedia>
                 <CardContent className={'content'}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        <a href={url}>
+                    <Typography gutterBottom variant="h6" component="h2">
+                        <a style={{color:"black",}} href={url}>
                             {name}
                         </a>
                     </Typography>
