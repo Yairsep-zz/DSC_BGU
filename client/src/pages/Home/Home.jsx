7707b3134d0c8b25e75edabd0a1ae2f93fd9807f
Roei Bracha <roei635@gmail.com>
2020-10-10 13:15:45 +0300

    fix the home screen

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,50 +6,53 @@ import ClubSocialIcons from '../../components/ClubSocialIcons'
 import Bubbles from '../../components/Bubbles/Bubbles'
 import Typography from "@material-ui/core/Typography";
 import Icon from "@material-ui/core/Icon";
+import {isMobile} from "../../utils/utils";
 
 function Home() {
   return (
-    <div className="container" style={{marginTop: "30px"}}>
-      <div className="row">
-        <div className="col-6">
-          <div className="leftSide-col">
-            <h2>What is DSC?</h2>
-            <p style={{ fontSize: 20, marginTop: 14, textAlign: 'left' }}>
-              Developer Student Clubs are university based community
-              groups for students interested in Google developer
-              technologies. Students from all undergraduate or
-              graduate programs with an interest in growing as a
-              developer are welcome.
-            </p>
-            <br />
-            <h2>Who can join DSC?</h2>
-            <p style={{ fontSize: 20, marginTop: 14, textAlign: 'left' }}>
-              We are looking for passionate students, who are self-motivated and eager to learn new Skills.
-              Prior coding experience is not mandatory.
-              If you have an idea for a great impactful project - DSC is your home!
-            </p>
-            <br />
-            <h2>Why should I join DSC?</h2>
-            <p style={{ fontSize: 20, marginTop: 14, textAlign: 'left' }}>
-              We are always being told we should "Step out of the crowd".
-              DSC is your path to do so.
-            </p>
-            <br />
-            <Link to="/Apply" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" size="large" color="secondary" endIcon={<Icon>send</Icon>}>
-                Join DSC BGU
-              </Button>
-            </Link>
+    <div className="home">
+      <div className={'logos'}>
+        <img alt="googleDevelopers" className="dsc" src="/pictures/logos/developer_student_clubs.png"/>
+        <img alt="googleDevelopers" className="googleDevelopers" src="/pictures/logos/google_developers.png"/>
+      </div>
+      <div className={'content'}>
+        <div className="left">
+          <h2>What is DSC?</h2>
+          <p style={{ fontSize: 20, marginTop: 14, textAlign: 'left' }}>
+            Developer Student Clubs are university based community
+            groups for students interested in Google developer
+            technologies. Students from all undergraduate or
+            graduate programs with an interest in growing as a
+            developer are welcome.
+          </p>
+          <br />
+          <h2>Who can join DSC?</h2>
+          <p style={{ fontSize: 20, marginTop: 14, textAlign: 'left' }}>
+            We are looking for passionate students, who are self-motivated and eager to learn new Skills.
+            Prior coding experience is not mandatory.
+            If you have an idea for a great impactful project - DSC is your home!
+          </p>
+          <br />
+          <h2>Why should I join DSC?</h2>
+          <p style={{ fontSize: 20, marginTop: 14, textAlign: 'left' }}>
+            We are always being told we should "Step out of the crowd".
+            DSC is your path to do so.
+          </p>
+          <br />
+          <Link to="/Apply" style={{ textDecoration: 'none' }}>
+            <Button variant="contained" size="large" color="secondary" endIcon={<Icon>send</Icon>}>
+              Join DSC BGU
+            </Button>
+          </Link>
+          <div className={'socialContainer'}>
             <ClubSocialIcons />
           </div>
         </div>
-        <div className="col-6">
-          <div className="rightSide-col">
-            <img alt="home_why" className="image" src="/pictures/content/home-why.png" />
-          </div>
+        <div className="right">
+          <img alt="home_why" className="image" src="/pictures/content/home-why.png" />
         </div>
       </div>
-      {/*<Bubbles />*/}
+      <Bubbles />
     </div>
   )
 }
